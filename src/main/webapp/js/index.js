a5d55eec8fdfd30bd74e4c788fed8e282cee58ec
branvogelsang <brian.andrew.vogelsang@gmail.com>
2020-04-13 17:03:20 -0400

    tier 5 finished

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,22 @@
+
+document.getElementById('submitBtn').addEventListener('click', getApi);
+
+function getApi() {
+    fetch('http://ec2-3-19-123-54.us-east-2.compute.amazonaws.com:9999/flashcard')
+        .then(response => response.json())
+        .then(json => implant(json));
+}
+
+function implant(json) {
+	
+	let searchId = document.getElementById('searchId').value;
+	
+	
+	document.getElementById('info').innerHTML = " " + json[searchId].id + " " +
+	json[searchId].category + " " +
+	json[searchId].question + " " +
+	json[searchId].answer + " ";
+	
+	console.log(json);
+}
+
---
+++ src/test/java/assessment1/JunitTest/JunitClass.java
@@ -0,0 +1,9 @@
+package assessment1.JunitTest;
+
+public class JunitClass {
+
+  public JunitClass() {
+    // TODO Auto-generated constructor stub
+  }
+
+}
